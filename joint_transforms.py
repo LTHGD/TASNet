85835a201b9a1bee143000428d7624b192918e17
LTHGD <95392713+LTHGD@users.noreply.github.com>
2021-12-13 15:53:57 +0800

    Create joint_transforms.py

---
+++ joint_transforms.py
@@ -0,0 +1,35 @@
+#!/usr/bin/env python3
+# -*- coding: utf-8 -*-
+"""
+Created on Tue Oct 12 14:35:27 2021
+
+@author: taiheng
+
+"""
+import random
+
+from PIL import Image
+
+class Compose(object):
+    def __init__(self, transforms):
+        self.transforms = transforms
+
+    def __call__(self, img, mask):
+        assert img.size == mask.size
+        for t in self.transforms:
+            img, mask = t(img, mask)
+        return img, mask
+
+class RandomHorizontallyFlip(object):
+    def __call__(self, img, mask):
+        if random.random() < 0.5:
+            return img.transpose(Image.FLIP_LEFT_RIGHT), mask.transpose(Image.FLIP_LEFT_RIGHT)
+        return img, mask
+
+class Resize(object):
+    def __init__(self, size):
+        self.size = tuple(reversed(size))  # size: (h, w)  PIL: (w, h)
+
+    def __call__(self, img, mask):
+        assert img.size == mask.size
+        return img.resize(self.size, Image.BILINEAR), mask.resize(self.size, Image.NEAREST)
